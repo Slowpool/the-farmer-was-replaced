587b2275c028377cad36f2e1551b60d8b52e5045
Slowpool <nevznachay02@gmail.com>
2026-08-15 09:40:58 +0300

    pumpkins algorithm. 90% efficiency

--- pumpkins.py
+++ pumpkins.py
@@ -1,74 +1,220 @@
 import utils
 
-relativeStartPoint = None
-fieldWidthForOneDrone = None
+FIELD_WIDTH_FOR_ONE_DRONE = get_world_size() / max_drones()
+NUMBER_OF_CELLS_PER_DRONE = get_world_size() * FIELD_WIDTH_FOR_ONE_DRONE
+
+LEFT_HORIZONTAL_BOUNDARY = 'LEFT_HORIZONTAL_BOUNDARY'
+NOT_HORIZONTAL_BOUNDARY = 'NOT_HORIZONTAL_BOUNDARY'
+RIGHT_HORIZONTAL_BOUNDARY = 'RIGHT_HORIZONTAL_BOUNDARY'
+
+MIN_THE_BIGGEST_PUMPKIN_PROBES = get_world_size() / 2
+
+Y_BOUNDARY_TRAVERSING_METHOD = 'ybtm'
+RANDOM_TRAVERSING_METHOD = 'rtm'
+FULL_TRAVERSING_METHOD = 'ftm'
+THE_BIGGEST_PUMPKIN_TRAVERSING_METHOD = Y_BOUNDARY_TRAVERSING_METHOD
+
+ownStartPoint = None
 deadPumpkins = []
 
 def startPumpking():
-    positionsForDrones = calculatePositionsForDrones()
-    print(positionsForDrones)
-    placeDrones(positionsForDrones)
-    plantPumpkinsAsMaster()
+	startPositionsForSubdrones = calculatePositionsForSubdrones()
+	placeSubrones(startPositionsForSubdrones)
+	plantPumpkinsAsMaster()
 
-def calculatePositionsForDrones():
-    global relativeStartPoint, fieldWidthForOneDrone
-    relativeStartPoint = utils.getCurrentPosition()
-    
-    fieldWidthForOneDrone = calculateFieldWidthForOneDrone()
+def calculatePositionsForSubdrones():
 
-    currentX = relativeStartPoint['x']
-    positionsForDrones = [relativeStartPoint]
-    for i in range(max_drones() - 1):
-        currentX += fieldWidthForOneDrone
-        nextDronePosition = (currentX, relativeStartPoint['y'])
-        positionsForDrones.append(nextDronePosition)
-    return positionsForDrones
+	global ownStartPoint
+	ownStartPoint = utils.getCurrentPosition()
 
-def calculateFieldWidthForOneDrone():
-    return get_world_size() / max_drones()
+	currentX = ownStartPoint['x']
+	startPositionsForSubdrones = []
+	for i in range(max_drones() - 1):
+		currentX += FIELD_WIDTH_FOR_ONE_DRONE
+		nextSubdronePosition = utils.newPoint(currentX, ownStartPoint['y'])
+		startPositionsForSubdrones.append(nextSubdronePosition)
+	return startPositionsForSubdrones
 
-def placeDrones():
-    # TODO
-    pass
 
-def plantPumpkinsAsSubordinate():
-    while True:
-        plantPumpkinsOnOwnTerritory()
-        traverseOwnDeadPumpkinsFirstTime()
-        while len(deadPumpkins) != 0:
-            traverseOwnDeadPumpkins()
-        # TODO help neighbour?
-        waitForTheBiggestPumpkinHarvesting()
+def placeSubrones(startPositionsForSubdrones):
+	for startPositionForSubdrone in startPositionsForSubdrones:
+		placeSubdrone(startPositionForSubdrone)
+
+def placeSubdrone(startPoint):
+	utils.moveToPosition(startPoint)
+	def plantPumpkinsAsSubdrone():
+		global ownStartPoint
+		ownStartPoint = startPoint
+		while True:
+			plantPumpkinsOnOwnTerritory()
+			traverseOwnDeadPumpkinsFirstTime()
+			while thereAreDeadPumpkins():
+				traverseOwnDeadPumpkins()
+			# TODO help neighbour?
+			waitForTheBiggestPumpkinHarvesting()
+	spawn_drone(plantPumpkinsAsSubdrone)
 
 def plantPumpkinsAsMaster():
-    while True:
-        plantPumpkinsOnOwnTerritory()
-        traverseOwnDeadPumpkinsFirstTime()
-        while len(deadPumpkins) != 0:
-            traverseOwnDeadPumpkins()
-        while theBiggestPumpkinIsGrown() != True:
-            observeTheBiggestPumpkin()
-        harvest()
+	global ownStartPoint
+	utils.moveToPosition(ownStartPoint)
+	theBiggestPumpkinProbes = []
+	while True:
+		plantPumpkinsOnOwnTerritory()
+		traverseOwnDeadPumpkinsFirstTime()
+		while thereAreDeadPumpkins():
+			traverseOwnDeadPumpkins()
+		while not theBiggestPumpkinIsGrown(theBiggestPumpkinProbes):
+			clearTheBiggestPumpkinProbes(theBiggestPumpkinProbes)
+			collectTheBiggestPumpkinInfo(theBiggestPumpkinProbes)
+		harvest()
+		clearTheBiggestPumpkinProbes(theBiggestPumpkinProbes)
 
 def plantPumpkinsOnOwnTerritory():
-    # TODO
-    pass
+	global ownStartPoint
+	utils.moveToPosition(ownStartPoint)
+	for i in range(NUMBER_OF_CELLS_PER_DRONE):
+		plantPumpkin()
+		goToNextCell()
+
+def plantPumpkin():
+	# TODO do it once in first traversing
+	if (get_ground_type() != Grounds.Soil):
+		till()
+	plant(Entities.Pumpkin)
+
+
+def goToNextCell():
+	# TODO it is not needed if you have 32 drones
+	horizontalBoundary = compareHorizontalBoundary()
+	if (horizontalBoundary == LEFT_HORIZONTAL_BOUNDARY):
+		if get_pos_y() % 2 == 0:
+			move(East)
+		else:
+			move(North)
+	elif (horizontalBoundary == NOT_HORIZONTAL_BOUNDARY):
+		if get_pos_y() % 2 == 0:
+			move(East)
+		else:
+			move(West)
+	elif (horizontalBoundary == RIGHT_HORIZONTAL_BOUNDARY):
+		if get_pos_y() % 2 == 0:
+			move(North)
+		else:
+			move(West)
+	else:
+		utils.moveTo(9)
+
+def compareHorizontalBoundary():
+	modX = get_pos_x() % FIELD_WIDTH_FOR_ONE_DRONE
+	if modX == 0:
+		return LEFT_HORIZONTAL_BOUNDARY
+	elif modX == (FIELD_WIDTH_FOR_ONE_DRONE - 1):
+		return RIGHT_HORIZONTAL_BOUNDARY
+	else:
+		return NOT_HORIZONTAL_BOUNDARY
 
 def traverseOwnDeadPumpkinsFirstTime():
-    # TODO
-    pass
+	global ownStartPoint
+	for i in range(NUMBER_OF_CELLS_PER_DRONE):
+		if (isDeadPumpkin()):
+			rememberDeadPumpkin()
+			plantPumpkin()
+		goToNextCell()
+
+def isDeadPumpkin():
+	return not can_harvest()
+
+def rememberDeadPumpkin():
+	deadPumpkin = utils.getCurrentPosition()
+	deadPumpkins.append(deadPumpkin)
+
+def thereAreDeadPumpkins():
+	return len(deadPumpkins) != 0
+
 def traverseOwnDeadPumpkins():
-    # TODO
-    pass
+	for deadPumpkinPosition in deadPumpkins:
+		utils.moveToPosition(deadPumpkinPosition)
+		if isDeadPumpkin():
+			plantPumpkin()
+		else:
+			forgetDeadPumpkin(deadPumpkinPosition)
+
+def forgetDeadPumpkin(position):
+	deadPumpkins.remove(position)
 
 def waitForTheBiggestPumpkinHarvesting():
-    # TODO
-    pass
+	while thereIsPumpkinBeneath():
+		pass
 
-def theBiggestPumpkinIsGrown():
-    # TODO
-    pass
+def thereIsPumpkinBeneath():
+	return get_entity_type() == Entities.Pumpkin
 
-def observeTheBiggestPumpkin():
-    # TODO
-    pass
+def theBiggestPumpkinIsGrown(theBiggestPumpkinProbes):
+	if not numberOfProbesIsEnough(theBiggestPumpkinProbes):
+		return False
+
+	prevPumpkinProbe = theBiggestPumpkinProbes[0]
+	for i in range(len(theBiggestPumpkinProbes) - 1):
+		if prevPumpkinProbe != theBiggestPumpkinProbes[i + 1]:
+			return False
+		else:
+			# theBiggestPumpkinProbes[i + 1] copy-pasted in optimization purposes
+			prevPumpkinProbe = theBiggestPumpkinProbes[i + 1]
+	return True
+
+def numberOfProbesIsEnough(theBiggestPumpkinProbes):
+	if THE_BIGGEST_PUMPKIN_TRAVERSING_METHOD == Y_BOUNDARY_TRAVERSING_METHOD:
+		isEnough = len(theBiggestPumpkinProbes) == 2
+	else:
+		isEnough = len(theBiggestPumpkinProbes) < MIN_THE_BIGGEST_PUMPKIN_PROBES
+
+	return isEnough
+
+
+# TODO does it work?
+def clearTheBiggestPumpkinProbes(theBiggestPumpkinProbes):
+	while len(theBiggestPumpkinProbes) != 0:
+		theBiggestPumpkinProbes.pop()
+
+def collectTheBiggestPumpkinInfo(theBiggestPumpkinProbes):
+	# collectTheBiggestPumpkinInfoRandomly()
+	# collectTheBiggestPumpkinInfoTraversing(theBiggestPumpkinProbes)
+	collectTheBiggestPumpkinInfoByYBoundaries(theBiggestPumpkinProbes)
+
+def collectTheBiggestPumpkinInfoTraversing(theBiggestPumpkinProbes):
+	for i in range(get_world_size()):
+		takePumpkinProbe(theBiggestPumpkinProbes)
+		move(North)
+
+def takePumpkinProbe(theBiggestPumpkinProbes):
+	theBiggestPumpkinProbes.append(measure())
+
+def collectTheBiggestPumpkinInfoByYBoundaries(theBiggestPumpkinProbes):
+	if bottomBoundaryIsCloser():
+		firstPosition = getBottomPosition()
+		secondPosition = getTopPosition()
+	else:
+		firstPosition = getTopPosition()
+		secondPosition = getBottomPosition()
+
+	utils.moveToPosition(firstPosition)
+	takePumpkinProbe(theBiggestPumpkinProbes)
+	utils.moveToPosition(secondPosition)
+	takePumpkinProbe(theBiggestPumpkinProbes)
+
+def getTopPosition():
+	global ownStartPoint
+	return {'x': ownStartPoint['x'], 'y': get_world_size() - 1}
+
+def getBottomPosition():
+	global ownStartPoint
+	return {'x': ownStartPoint['x'], 'y': 0}
+
+def bottomBoundaryIsCloser():
+	return get_pos_y() < get_world_size() / 2
+
+harvest()
+clear()
+change_hat(Hats.Carrot_Hat)
+if __name__ == '__main__':
+	startPumpking()
